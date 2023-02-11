8748fa195b5cc5ac4d0ca4f2ef1f4239a58169a1
NRDsstuff <nrdfoss@gmail.com>
2023-02-11 20:07:06 +0100

    small changes

--- loqa/backend/other/loqa/models/message.js
+++ loqa/backend/other/loqa/models/message.js
@@ -2,7 +2,7 @@ const mongus = require('mongoose')
 
 const msgSchema = new mongus.Schema({
 
-    posterID: {type:String, required:true},
+    userID: {type:String, required:true},
     content: {type:String, required:true, minlength:2, maxlength:2000},
     channelID: {type:String, required:true},
     reactions: {type:Array, required:false, default:[]},
